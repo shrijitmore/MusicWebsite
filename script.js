e74eeba7a757df958ce986f363342dfc16352bf2
Shrijit <Shrijitmore556@gmail.com>
2022-10-02 13:08:40 +0530

    Changes Updated

--- script.js
+++ script.js
@@ -43,6 +43,26 @@ audioElement.addEventListener('timeupdate',()=>{
     // update seekbar
     progress = parseInt((audioElement.currentTime/audioElement.duration)*100)
     myProgressBar.value = progress
+    if(myProgressBar.value == "100"){
+        if(songIndex >= 5){
+            songIndex = 0;
+        }
+        else{
+            songIndex += 1;
+        }
+        document.getElementById(`${(songIndex)}`).classList.remove('fa-play-circle');
+        document.getElementById(`${(songIndex)}`).classList.add('fa-pause-circle');
+        document.getElementById(`${Math.abs(songIndex-1)}`).classList.remove('fa-pause-circle');
+        document.getElementById(`${Math.abs(songIndex-1)}`).classList.add('fa-play-circle')
+    
+        audioElement.src = `songs/${songIndex}.mp3`;
+        songnames.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        mygif.style.opacity = 1;
+        Play.classList.remove('fa-play-circle');
+        Play.classList.add('fa-pause-circle');
+    }
 })
 
 myProgressBar.addEventListener('change', ()=>{
@@ -63,16 +83,16 @@ Array.from(document.getElementsByClassName("itemSong")).forEach((element)=>{
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
         play();
-        songnames.innerText = songs[songIndex-1].songName;
-        audioElement.src = `songs/${songIndex-1}.mp3`;
+        songnames.innerText = songs[songIndex].songName;
+        audioElement.src = `songs/${songIndex}.mp3`;
         audioElement.currentTime = 0;
         audioElement.play();
         }else{
         e.target.classList.remove('fa-pause-circle');
         e.target.classList.add('fa-play-circle');
         pause()
-        songnames.innerText = songs[songIndex-1].songName;
-        audioElement.src = `songs/${songIndex-1}.mp3`;
+        songnames.innerText = songs[songIndex].songName;
+        audioElement.src = `songs/${songIndex}.mp3`;
         audioElement.currentTime = 0;
         audioElement.pause();
         }
